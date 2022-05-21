b0499081057656684a838cf7e4d57275d2781515
Alexis Ortega <alexis.oc3@gmail.com>
2022-05-20 20:08:40 -0600

    Add players component

---
+++ src/features/players/components/PlayerListItem/PlayerListItem.js
@@ -0,0 +1,9 @@
+import "./PlayerListItem.css"
+
+export function PlayerListItem(props) {
+    return (
+        <div className="player-list-item">
+            <p id={`player_${props.player.id}`}>{props.player.first_name} {props.player.last_name}</p>
+        </div>
+    )
+}
---
+++ src/features/players/components/PlayerListItem/index.js
@@ -0,0 +1,1 @@
+export { PlayerListItem } from './PlayerListItem'
