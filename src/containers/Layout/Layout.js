f90f9682d74f586b8c7cd23ccebc7088e97c9cdb
Alexis Ortega <alexis.oc3@gmail.com>
2022-05-22 15:26:50 -0600

    Add layout container

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,5 @@
+import './Layout.css';
+
+export function Layout({children }) {
+  return <div>{children}</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Layout } from './containers/Layout';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +10,9 @@ root.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>
-  <App />
+  <Layout>
+    <App />
+  </Layout>
 );
 
 // If you want to start measuring performance in your app, pass a function
