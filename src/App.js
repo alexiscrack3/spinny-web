da42937e6a3e31629e70f14cae675a3f3f692c50
Alexis Ortega <alexis.oc3@gmail.com>
2022-05-22 18:48:42 -0600

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Players } from "./containers/Players";
+import { Home } from "./containers/Home";
 import "./App.css";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/players" element={<Players />} />
       </Routes>
     </BrowserRouter>
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,7 @@
+import "./Home.css";
+
+export function Home() {
+    return (
+        <div><p>Home page</p></div>
+    )
+}
