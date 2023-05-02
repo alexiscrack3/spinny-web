056a33d73d50e28455177fb2edf254f9aeb9cf36
Alexis Ortega <alexis.oc3@gmail.com>
2023-05-01 22:23:53 -0600

    Update header

--- src/pages/Layout/Header.js
+++ src/pages/Layout/Header.js
@@ -1,18 +1,20 @@
-import React from "react";
-import Nav from "react-bootstrap/Nav";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export function Header() {
   return (
-    <Nav>
-      <Nav.Item>
-        <Nav.Link href="/home">Home</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/players">Players</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/about">About</Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/home">Spinny</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/players">Players</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
