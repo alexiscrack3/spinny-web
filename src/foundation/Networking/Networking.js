339841583771af00a9b86406d1388d623bac22a1
Alexis Ortega <alexis.oc3@gmail.com>
2023-06-11 19:20:49 -0600

    Set cookies in requests automatically

--- src/foundation/Networking/Networking.js
+++ src/foundation/Networking/Networking.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const InitializeAxios = () => {
   axios.defaults.baseURL =
     process.env.REACT_APP_BASE_URL || 'http://localhost:3000';
+  axios.defaults.withCredentials = true;
 };
 
 export default InitializeAxios;
